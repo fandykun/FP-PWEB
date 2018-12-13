60408e5ba0d44f6ea374776ec4247ba889067ceb
Lix <17717036+pradiptlx@users.noreply.github.com>
2018-12-13 23:54:31 +0700

    Fix layout product page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,20 +6,25 @@
             <a href="/product/create" class="btn btn-warning my-3">Create Product</a>
         @endif
     @endif
-    <div class="card-deck">
+    <div class="card-columns">
         @foreach($products as $product)
+        
             <div class="card">
-                <img src="" alt="temporary-image" class="card-img-top">
-                <div class="card-body">
-                    {{-- Jangan lupa pake foreach --}}
-                    
-                    <h4 class="card-title">{{$product->title}}</h4>
-                    <p class="card-text">{{$product->description}}</p>  
-                </div>
+                <a href="/product/{{$product->id}}">
+                    <img src="/storage/coverProducts/noimage.jpg" alt="temporary-image" class="card-img-top">
+                    <div class="card-body">
+                        {{-- Jangan lupa pake foreach --}}
+                        
+                        <h4 class="card-title text-dark">{{$product->title}}</h4>
+                        <p class="card-text text-dark">{{$product->description}}</p>  
+                    </div>
+                </a>
+                
                 <div class="card-footer">
                     {{-- Href masih belum fix --}}
-                    <a href="/product/{{$product->id}}" class="btn btn-success my-2 my-sm-0"></a>
-                    {{-- Tambah button untuk cart tanpa harus visit show product --}}
+                    {{-- <a href="/transaction/{{$transaction->id}}" class="btn btn-success my-2 my-sm-0">Buy</a> --}}
+                    {{-- Tambah button untuk buy (redirect ke transaksi) tanpa harus visit show product --}}
+                    <h4 class="card-text text-dark">Rp {{$product->price}}</h4>
                 </div>
             </div>
         @endforeach
