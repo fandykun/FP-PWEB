aff9dcb49f36364276194988d35fd5c29dd2ca6f
Lix <17717036+pradiptlx@users.noreply.github.com>
2018-12-15 00:41:45 +0700

    Menambahkan carousel
    Masih ngebug, logic sudah bisa

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,6 +6,43 @@
             <a href="/product/create" class="btn btn-warning my-3">Create Product</a>
         @endif
     @endif
+
+    <div class="carousel slide my-4" data-ride="carousel" id="carouselProduct">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselProduct" data-slide-to="0" class="active"></li>
+            @for($i = 1; $i < count($products); $i++)
+                <li data-target="#carouselProduct" data-slide-to="{{$i}}"></li>
+            @endfor
+        </ol>
+            
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/storage/coverProducts/{{$products[0]->coverProducts}}" alt="{{$products[0]->productName}}" class="d-block" style="height:50%; width:50%; background-size:100%;">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>{{$products[0]->productName}}</h5>
+                    <p>{{$products[0]->description}}</p>
+                </div>
+            </div>
+            @foreach ($products as $product)
+                <div class="carousel-item">
+                    <img src="/storage/coverProducts/{{$product->coverProducts}}" alt="{{$product->productName}}" class="d-block"  style="height:50%; width:50%; background-size:100%;">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>{{$product->productName}}</h5>
+                        <p>{{$product->description}}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+         <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#carouselProduct" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#carouselProduct" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+    </div>
+
     <div class="card-columns">
         @foreach($products as $product)
             {{-- OPTIONAL: Cleanup style maybe with css file --}}
